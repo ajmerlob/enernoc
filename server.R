50910295018908e2e29669abb60aa6edf756d33e
Mountain 213 <aaron@mountain213.com>
2014-01-20 22:06:05 -0500

    updating R files again

--- server.R
+++ server.R
@@ -2,9 +2,7 @@ library(shiny)
 library(TTR)
 
 ##Oh hai!!
-add_stuff = function(ost2){
-  ost = ost2
-  #cat("\nadding stuff\n")
+append_datetime = function(ost){
   ost$Year <- substr(ost$dttm_utc,0,4)
   ost$Month <- substr(ost$dttm_utc,6,7)
   ost$Day <-substr(ost$dttm_utc,9,10)
@@ -17,16 +15,13 @@ add_stuff = function(ost2){
 } 
 
 get_slice <- function(ost,boundaries){
-#   cat("Slice\n")
   ost_len = nrow(ost)
-#   cat(ost_len)
-#   cat("\n")
   ost_min = as.integer(ost_len * min(boundaries) / 100)
   ost_max = as.integer(ost_len * max(boundaries) / 100)
   return(ost[ost_min:ost_max,])
 }
 
-get_slice_and_plot <- function(ost2,aggtype,timeline,boundaries,abs_rel){
+get_dynamic_plot <- function(ost2,aggtype,timeline,boundaries,abs_rel){
   ost_min = min(ost2$value)
   ost_max = max(ost2$value)
   
@@ -101,9 +96,9 @@ get_slice_and_plot <- function(ost2,aggtype,timeline,boundaries,abs_rel){
     }
   } else{
     if(abs_rel){
-      return(plot(agg_plot,sub=ost_sub,type="p",xlab=timeline,ylab="Total Demand",main="Demand Over Time",ylim=c(ost_min,ost_max)))
+      return(plot(agg_plot,sub=ost_sub,type="p",xlab=timeline,ylab="Average Total Demand",main="Demand Over Time",ylim=c(ost_min,ost_max)))
     } else {
-      return(plot(agg_plot,sub=ost_sub,type="p",xlab=timeline,ylab="Total Demand",main="Demand Over Time"))
+      return(plot(agg_plot,sub=ost_sub,type="p",xlab=timeline,ylab="Average Total Demand",main="Demand Over Time"))
     }
   }
 }
@@ -115,19 +110,13 @@ show_trend <- function(ost2,boundaries){
   return(ost_dec)
 }
 
-get_arima <- function(ost2,boundaries){
-  ost = get_slice(ost2,boundaries)
-  ost_ts_diff = diff(ts(ost$value,frequency=2016))
-  return(ost_ts_diff)
-}
-
 shinyServer(function(input,output){
   output$Caption1 = renderText({
     paste(paste("Filename:",input$filename,sep=" "), "|",paste("Day(s): ",input$weekday,sep=" "),"|",paste("Aggregation:",input$aggtype,sep=" "),"|",paste("Date Range: ",paste(input$date_range,collapse="% - "),"%",sep=""),sep=" ")
   })
   
   ost <- reactive({
-    data = add_stuff(read.csv(paste("/home/rstudio_user/enernoc/csv/",input$filename,sep="")))
+    data = append_datetime(read.csv(paste("/home/rstudio_user/enernoc/csv/",input$filename,sep="")))
   })
   
   output$Exp1 = renderText({
@@ -136,7 +125,7 @@ shinyServer(function(input,output){
   
   output$agg_plot = renderPlot({
     data <- ost()
-    get_slice_and_plot(data,input$weekday,input$aggtype,input$date_range,input$abs_rel)
+    get_dynamic_plot(data,input$weekday,input$aggtype,input$date_range,input$abs_rel)
   })    
   
   output$Exp2 = renderText({
@@ -150,7 +139,7 @@ shinyServer(function(input,output){
   })
   
   output$Exp3 = renderText({
-    "Unfortunately, for a time series with seasonality (expressed in the hourly and daily cycles mentioned before), these summary statistics are not particularly helpful in thinking about the data.  One first step we might take is to visualize and make an adjustment to separate the seasonality from the rest of the data.  If the fluctuations in demand values seem to be fairly consistent in magnitude over time (and the seasonality seems to be fairly consistent in magnitude both over time and relative to the level of overall demand), we can consider using an additive model to decompose the time series into components.  The following graph shows the time series decomposed into ‘trend,’ ‘seasonal,’ and ‘random’ components, which represent the changes in the average level of demand, the repeated fluctuations observed at different points in time, and fluctuations that are not predicted by either of the first two components, but were observed at a specific time, respectively.  [[Click 'Decompose Timeseries' on left sidebar to see graphs]]"
+    "Unfortunately, for a time series with seasonality (expressed in the hourly and daily cycles mentioned before), these summary statistics are not particularly helpful in thinking about the data.  One first step we might take is to make an adjustment to separate the seasonality from the rest of the data (and try to visualize the seasonality).  If the fluctuations in demand values seem to be fairly consistent in magnitude over time (and the seasonality seems to be fairly consistent in magnitude both over time and relative to the level of overall demand), we can consider using an additive model to decompose the time series into components.  The following graph shows the time series decomposed into ‘trend,’ ‘seasonal,’ and ‘random’ components, which represent the changes in the average level of demand, the repeated fluctuations observed at different points in time, and fluctuations that are not predicted by either of the first two components, but were observed at a specific time, respectively.  [[Click 'Decompose Time Series' on left sidebar to see graphs]]"
   })
   
   decom = reactive({
@@ -165,82 +154,130 @@ shinyServer(function(input,output){
   })
   
   output$Exp4 = renderText({
-    "Here is a look at the time seasonally adjusted time series:"
+    "To the extent that an additive model captures the time series, the trend component above shows how the overall level of demand changes over the course of time.  We can try to improve our understanding of how demand changes over time by using more sophisticated models.  Let's run some diagnostics on the seasonally adjusted time series (pictured below)."
   })
+
+  
+  ts_seasonally_adjusted_t = reactive({
+    if (input$trend){
+      ts(decom()$x-decom()$seasonal,frequency=2016)
+    }  else {}
+  })
+  
+  ts_seasonally_adjusted_c = reactive({
+    if (input$complex){
+      ts(decom()$x-decom()$seasonal,frequency=2016)
+    }  else {}
+  })
+  
+  ts_seasonally_adjusted_a = reactive({
+    if (input$arima){
+      ts(decom()$x-decom()$seasonal,frequency=2016)
+    }  else {}
+  })
+  
+  
   
   output$ts_minus_seasonal = renderPlot({
     if (input$trend){
-      plot(decom()$x-decom()$seasonal)
+      plot(ts_seasonally_adjusted_t(),main="Seasonally Adjusted Demand",xlab="Observations Since Start of Date Range",ylab="Seasonally Adjusted Demand")
     }  else {}
   })
   
-  output$Exp4 = renderText({
-    "To the extent that an additive model captures the time series, the trend component above shows how the overall level of demand changes over the course of time.  We can try to improve our understanding of how demand changes over time by using a more sophisticated model."
+  output$Exp5 = renderText({
+    "The ACF and PACF can help us identify modeling changes that can improve the fit.  These are pictured below.  The ACF shows the degree to which values of a time series are correlated with the values that came before.  Since any correlation between a value and it’s immediately preceding value would tend to propagate backwards through earlier and earlier values, the Partial ACF is useful to find the autocorrelations not accounted for by this propagation."
   })
   
-  output$ts_decomp = renderPlot({
-    if (input$trend){
-      plot(decom())
-    }  else {}
-  })
-  
-  
-  
-
-  
-  
-  
-  
-  
-  
-  
-  
-
   output$decomp_acf = renderPlot({
     if (input$trend){
-      plot(acf(ts(decom()$random,frequency=2016),na.action=na.contiguous,lag.max=30),
+      plot(acf(ts_seasonally_adjusted_t(),na.action=na.contiguous,lag.max=30),
+           main="Autocorrelation of Seasonally Adjusted Demand",
            sub="Lags are expressed as a fraction of a week (2016 observations)"
            )
     } else {}
   })
   output$decomp_pacf = renderPlot({
     if (input$trend){
-      plot(pacf(ts(decom()$random,frequency=2016),na.action=na.contiguous,lag.max=30),
+      plot(pacf(ts_seasonally_adjusted_t(),na.action=na.contiguous,lag.max=30),
+           main="Partial Autocorrelation of Seasonally Adjusted Demand",
            sub="Lags are expressed as a fraction of a week (2016 observations)"
            )
     } else {}
   })
   
-  diff_plot = reactive({
-    ts(get_arima(ost(),input$date_range),frequency=2016)
+  output$Exp6 = renderText({
+    "When the ACF shows large autocorrelations tapering off over many lags, one approach is the difference the time series.  Differencing adds the previous demand value as a predictor of the current demand value.  Most of the files display this signature, so the differenced values are visualized below. [[Click 'Difference Model' on left sidebar to see graphs]]"
   })
   
-  output$auto_arima = renderPlot({
+  diff_plot <- reactive({
     if (input$complex){
-      plot(diff_plot())
+    diff(ts_seasonally_adjusted_c())
     } else {}
   })
-  output$arima_acf = renderPlot({
+  
+  output$diff_plot = renderPlot({
+    if (input$complex){
+      plot(diff_plot(),main='Differenced Seasonally Adjusted Demand',ylab="Differenced Seasonally Adjusted Demand",xlab="Time in Weeks")
+    } else {}
+  })
+  
+  output$diff_acf = renderPlot({
     if (input$complex){
       plot(acf(diff_plot(),na.action=na.contiguous,lag.max=30),
+           main="Autocorrelation of Differenced Seasonally Adjusted Demand",
            sub="Lags are expressed as a fraction of a week (2016 observations)"
       )
     } else {}
   })
-  output$arima_pacf = renderPlot({
+  output$diff_pacf = renderPlot({
     if (input$complex){
       plot(pacf(diff_plot(),na.action=na.contiguous,lag.max=30),
+           main="Partial Autocorrelation of Differenced Seasonally Adjusted Demand",
            sub="Lags are expressed as a fraction of a week (2016 observations)"
       )
     } else {}
   })
   
-  
-#   browser()
-#   output$caption = renderText({
-#     formulaText()
-#   })
+  output$Exp7 = renderText({
+    "If the differenced time series looks like white noise, and the associated ACF and PACF do not have any significant values in their signatures, then we have built an adequate model from the time series data, and would want to pursue other predictors.  On the other hand, if the ACF of PACF still show significant values (more lines above the blue dotted line than would be expected by chance), than we can attempt to find a better fit by adding an autoregressive feature or a moving average feature to our model (either in addition to, or instead of the difference term in our model). [[Click ‘Fit ARIMA’ on the left sidebar to fit an ARIMA model]]"
+  })
   
   
+  arima_model = reactive({
+    if (input$arima){
+      auto.arima(ts_seasonally_adjusted_a(),stationary=TRUE,seasonal=FALSE,
+                 max.d=2,
+                 max.p=3,
+                 max.q=3
+                 )
+    } else {}
+  })
   
+  output$arima_text <- renderPlot({
+    if (input$arima){
+      cat("starting")
+      plot(forecast(arima_model(),h=5),xlab="Time in Weeks",ylab="Demand",col="red")
+      par(new=TRUE)
+      plot(ts_seasonally_adjusted_a(),xlab="",ylab="",col="black")
+      cat("ending")
+    } else {}
+  })
+  
+  output$Exp8 = renderText({
+    "The ARIMA model above was chosen because it minimized the AIC (Akaike information criterion), a metric that can be used to compare the fit of different ARIMA models (in relative terms, not in absolute terms). In a more sophisticated setting, a custom error metric might be used that incorporated a closer approximation of the cost of the error.  For example, a custom error metric might assign a larger loss when the model underestimated demand versus when the model overestimated demand, because it is more costly to generate additional electricity to meet higher-than-expected demand than it is to reduce electricity output.  The forecasts from the ARIMA model are shown in red alongside the actual demand shown in black."
+  })
+  
+  output$Caption2 = renderText({
+    "Easter Egg - Did You Know?"
+  })
+  
+  output$Exp9 = renderText({
+    "The ARIMA model above was chosen because it minimized the AIC (Akaike information criterion), a metric that can be used to compare the fit of different ARIMA models (in relative terms, not in absolute terms). In a more sophisticated setting, a custom error metric might be used that incorporated a closer approximation of the cost of the error.  For example, a custom error metric might assign a larger loss when the model underestimated demand versus when the model overestimated demand, because it is more costly to generate additional electricity to meet higher-than-expected demand than it is to reduce electricity output.  The forecasts from the ARIMA model are shown in red alongside the actual demand shown in black."
+  })
+  
+  output$easter_plot <- renderPlot({
+    if(input$easter){
+      agg_plot <- aggregate(ost[ost$Weekday=="Saturday",]$value,by=list(ost[ost$Weekday=="Saturday",]$Hour), FUN=mean)
+    } else {}
+  })
 })
--- ui.R
+++ ui.R
@@ -19,31 +19,42 @@ shinyUI(pageWithSidebar(
     ,
     checkboxInput("abs_rel","Set static min and max demand on Y-axis",FALSE)
     ,
-    checkboxInput("trend","Decompose Timeseries",FALSE)
+    checkboxInput("trend","Decompose Time Series",FALSE)
     ,
-    checkboxInput("complex","Fit more complex models",FALSE)
-    
-    
+    checkboxInput("complex","Difference Model",FALSE)
+    ,
+    checkboxInput("arima","Fit ARIMA",FALSE)
+    ,
+    checkboxInput("easter","Easter Egg",TRUE)
   ),
 
   mainPanel(
-     h3(textOutput("Caption1")) ,
-     
+    h3(textOutput("Caption1")) ,
     
     h4(textOutput("Exp1")),
     plotOutput("agg_plot"),
     h4(textOutput("Exp2")),
     verbatimTextOutput("summary1"),
+    
     h4(textOutput("Exp3")),
     plotOutput("ts_decomp"),
     h4(textOutput("Exp4")),
-    plotOutput("ts_minus_seasonal")#,
-#     plotOutput("decomp_acf"),
-#     plotOutput("decomp_pacf"),
-#     plotOutput("auto_arima"),
-#     plotOutput("arima_acf"),
-#     plotOutput("arima_pacf")
+    plotOutput("ts_minus_seasonal"),
+    h4(textOutput("Exp5")),
+    plotOutput("decomp_acf"),
+    plotOutput("decomp_pacf"),
+
+    h4(textOutput("Exp6")),
+    plotOutput("diff_plot"),
+    plotOutput("diff_acf"),
+    plotOutput("diff_pacf"),
     
+    h4(textOutput("Exp7")),
+    plotOutput("arima_text"),
+    h4(textOutput("Exp8")),
+    
+    h3(textOutput("Caption2")) ,
+    h4(textOutput("Exp9"))
   )
 ))
 
